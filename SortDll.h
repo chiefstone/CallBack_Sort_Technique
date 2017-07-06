d31371f1df09b2c7ee76646944d2355d52e78e70
Andres R <xnorlogic@gmail.com>
2017-07-06 16:07:55 -0500

    Update SortDll.h

--- SortDll.h
+++ SortDll.h
@@ -9,11 +9,18 @@
 
 typedef unsigned char byte; //0 to 255
 
-typedef int (__stdcall *CompareFunction)(const byte*, const byte*); //Prototype for callback: 
+//Prototype for callback----------------------------------------------------------------------------------
+//Takes in two bytes that contain the memory address of components to be compared. This CallBack will be
+//used on any of the other techniques to sort.
+typedef int (__stdcall *CompareFunction)(const byte*, const byte*); 
+//--------------------------------------------------------------------------------------------------------
 																	                                  //Compare memory contents
-
-//Bubble sort Prototype------------------------------------------------------------------------------
-void SORTDLL_API __stdcall Bubblesort(byte* array, int size, int elem_size, CompareFunction cmpFunc);
-//---------------------------------------------------------------------------------------------------
+//Bubble sort Prototype-----------------------------------------------------------------------------------
+//Bubblesort_1(<byte*: "memory location of the data to be sorted">,
+//             <int: "number of elements in the array to be sorted">,
+//             <int: "size of the data type contained in the array to be sorted">
+//             <CompareFunction : "CallBack to the function that will compare data in the array">)
+void SORTDLL_API __stdcall Bubblesort_1(byte* Mem_Loc, int size, int sizeD, CompareFunction CMP_funct);
+//--------------------------------------------------------------------------------------------------------
 
 #endif
